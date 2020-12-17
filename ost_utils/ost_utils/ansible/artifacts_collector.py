e0e93bed9f968363923c6f4423fd62ed5db07c2d
Marcin Sobczyk <msobczyk@redhat.com>
2020-12-17 13:12:27 +0100

    ost_utils: Introduce 'ansible.patterns' module
    This patch introduces an 'ansible.patterns' module, which provides
    patterns that can be passed to ansible, to work on specific VMs.
    
    Until now we had the patterns defined in the same module as fixtures,
    which limited the ansible usage to fixtures only. To get rid of this
    limitation, we:
    - expanded the 'backend' module to provide VMs' hostnames,
    - added 'ansible.patterns' module, which uses the hostnames returned
      by 'backend' and creates ansible patterns from them
    
    Change-Id: Iaa947566164987c299c88bb3fd002396f4b8e589
    Signed-off-by: Marcin Sobczyk <msobczyk@redhat.com>

--- ost_utils/ost_utils/ansible/artifacts_collector.py
+++ ost_utils/ost_utils/ansible/artifacts_collector.py
@@ -22,6 +22,7 @@ import os
 import yaml
 
 from ost_utils.ansible import module_mapper as mm
+from ost_utils.ansible import patterns
 from ost_utils import shell
 
 
@@ -29,9 +30,6 @@ SUITE_PATH = os.environ.get('SUITE')
 _ENGINE = 'engine'
 _HOST0 = 'host-0'
 _HOST1 = 'host-1'
-_LAGO_ENGINE_PATTERN = '~lago-.*-' + _ENGINE
-_LAGO_HOST0_PATTERN = '~lago-.*-' + _HOST0
-_LAGO_HOST1_PATTERN = '~lago-.*-' + _HOST1
 
 
 class ArtifactsCollector(object):
@@ -79,16 +77,16 @@ class ArtifactsCollector(object):
 class EngineArtifactsCollector(ArtifactsCollector):
     def __init__(self, artifacts_dir):
         super(EngineArtifactsCollector, self).__init__(
-            _LAGO_ENGINE_PATTERN, _ENGINE, artifacts_dir)
+            patterns.engine(), _ENGINE, artifacts_dir)
 
 
 class Host0ArtifactsCollector(ArtifactsCollector):
     def __init__(self, artifacts_dir):
         super(Host0ArtifactsCollector, self).__init__(
-            _LAGO_HOST0_PATTERN, _HOST0, artifacts_dir)
+            patterns.host0(), _HOST0, artifacts_dir)
 
 
 class Host1ArtifactsCollector(ArtifactsCollector):
     def __init__(self, artifacts_dir):
         super(Host1ArtifactsCollector, self).__init__(
-            _LAGO_HOST1_PATTERN, _HOST1, artifacts_dir)
+            patterns.host1(), _HOST1, artifacts_dir)
