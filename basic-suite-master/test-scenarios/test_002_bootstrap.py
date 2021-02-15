7f8736aea0d06d2b14293efcf3fc0a22314b536a
Yedidyah Bar David <didi@redhat.com>
2021-02-15 15:21:51 +0200

    do not import defaults, allow override
    Change-Id: I9d5823b361458efe112a5057395ea740e7e8e26a
    Signed-off-by: Yedidyah Bar David <didi@redhat.com>

--- basic-suite-master/test-scenarios/test_002_bootstrap.py
+++ basic-suite-master/test-scenarios/test_002_bootstrap.py
@@ -45,7 +45,6 @@ from ost_utils import general_utils
 from ost_utils.pytest import order_by
 from ost_utils.pytest.fixtures import root_password
 from ost_utils.pytest.fixtures.ansible import *
-from ost_utils.pytest.fixtures.defaults import *
 from ost_utils.pytest.fixtures.engine import *
 from ost_utils.pytest.fixtures.network import storage_network_name
 from ost_utils.pytest.fixtures.virt import *
