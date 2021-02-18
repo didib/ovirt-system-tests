38903b4a17f6b597df565b7cb88e13398d3aa530
Yedidyah Bar David <didi@redhat.com>
2021-02-18 09:56:07 +0200

    Unite basic-suite-master 002_ and 004_ with HE
    This patch:
    
    - Adapts basic-suite's 002_ and 004_ tests to be able to be used as-is
    also in he-basic-suite
    
    - Replaces he-'s 002_ and 004_ with links to basic-
    
    - Adapts conftest.py in each of them accordingly - mainly, to override
    some common fixtures as needed
    
    - Links also files/test-vm.ovf to he-, for test_ovf_import. Should
    probably be moved to some common location. TODO.
    
    Change-Id: Ied836cda6b622dbebdb869e0b83fa4c0e0b7ca2c
    Signed-off-by: Yedidyah Bar David <didi@redhat.com>

--- basic-suite-master/test-scenarios/conftest.py
+++ basic-suite-master/test-scenarios/conftest.py
@@ -19,10 +19,14 @@
 # Refer to the README and COPYING files for full details of the license
 #
 
+import os
+import pytest
+
 from ost_utils.pytest import pytest_collection_modifyitems
 
 from ost_utils.pytest.fixtures.ansible import ansible_clean_private_dirs
 from ost_utils.pytest.fixtures.ansible import ansible_collect_logs
+from ost_utils.pytest.fixtures.ansible import ansible_engine
 
 from ost_utils.pytest.fixtures.artifacts import artifacts
 from ost_utils.pytest.fixtures.artifacts import artifacts_dir
@@ -31,7 +35,43 @@ from ost_utils.pytest.fixtures.artifacts import collect_artifacts
 from ost_utils.pytest.fixtures.backend import backend
 from ost_utils.pytest.fixtures.backend import hosts_hostnames
 
-from ost_utils.pytest.fixtures.defaults import hostnames_to_add
-from ost_utils.pytest.fixtures.defaults import hostnames_to_reboot
+from ost_utils.pytest.fixtures.defaults import *
+
+from ost_utils.pytest.fixtures.storage import *
 
 from ost_utils.pytest.running_time import *
+
+
+@pytest.fixture(scope="session")
+def sd_iscsi_host_ips(engine_storage_ips): # pylint: disable=function-redefined
+    return engine_storage_ips
+
+
+@pytest.fixture(scope="session")
+def sd_nfs_host_storage_ip(engine_storage_ips): # pylint: disable=function-redefined
+    return engine_storage_ips[0]
+
+
+@pytest.fixture(scope="session")
+def sd_iscsi_ansible_host(ansible_engine): # pylint: disable=function-redefined
+    return ansible_engine
+
+
+if os.environ.get("USE_LAGO_OST_PLUGIN", "0") == "1":
+    import ovirtlago
+    import ovirtlago.prefix
+    import pytest
+    from ovirtlago import testlib
+
+    @pytest.fixture(scope="session")
+    def prefix():
+        yield testlib.get_test_prefix()
+
+    @pytest.fixture(autouse=True, scope="session")
+    def repo_server(prefix):
+        with ovirtlago.server.repo_server_context(
+            gw_ip=prefix.virt_env.get_net().gw(),
+            port=ovirtlago.constants.REPO_SERVER_PORT,
+            root_dir=prefix.paths.internal_repo()
+        ):
+            yield
--- he-basic-suite-master/test-scenarios/conftest.py
+++ he-basic-suite-master/test-scenarios/conftest.py
@@ -21,12 +21,48 @@
 
 import pytest
 
+from ost_utils import engine_object_names
+
 from ost_utils.pytest import pytest_collection_modifyitems
 
 from ost_utils.pytest.fixtures.artifacts import artifacts_dir
 from ost_utils.pytest.fixtures.ansible import *
 from ost_utils.pytest.fixtures.backend import *
+from ost_utils.pytest.fixtures.defaults import *
 from ost_utils.pytest.fixtures.engine import *
 from ost_utils.pytest.fixtures.network import *
 from ost_utils.pytest.fixtures.sdk import *
+from ost_utils.pytest.fixtures.storage import *
 from ost_utils.pytest.running_time import *
+
+
+# hosted-engine suites use a separate storage VM, but use the management
+# network for storage traffic. Override the relevant fixtures.
+
+
+@pytest.fixture(scope="session")
+def sd_iscsi_host_ips(storage_management_ips): # pylint: disable=function-redefined
+    return storage_management_ips
+
+
+@pytest.fixture(scope="session")
+def sd_nfs_host_storage_ip(storage_management_ips): # pylint: disable=function-redefined
+    return storage_management_ips[0]
+
+
+@pytest.fixture(scope="session")
+def sd_iscsi_ansible_host(ansible_storage): # pylint: disable=function-redefined
+    return ansible_storage
+
+
+# hosted-engine suites use the default cluster/dc names. Override.
+
+
+@pytest.fixture(scope="session")
+def ost_dc_name(): # pylint: disable=function-redefined
+    return engine_object_names.DEFAULT_DC_NAME
+
+
+@pytest.fixture(scope="session")
+def ost_cluster_name(): # pylint: disable=function-redefined
+    return engine_object_names.DEFAULT_CLUSTER_NAME
